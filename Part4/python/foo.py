e3e32a9493de12e727274d4b9c258ae482c5138f
stefalarse <60001631+stefalarse@users.noreply.github.com>
2020-01-17 17:03:50 +0100

    Update foo.py

--- Part4/python/foo.py
+++ Part4/python/foo.py
@@ -12,20 +12,24 @@ i = 0
 def incrementingFunction():
     global i
     # TODO: increment i 1_000_000 times
+    for x in range(1, 1000000):
+        i = i + 1;
 
 def decrementingFunction():
     global i
     # TODO: decrement i 1_000_000 times
-
-
+    for x in range(1, 1000000):
+        i = i - 1;
 
 def main():
     # TODO: Something is missing here (needed to print i)
 
-    incrementing = Thread(target = incrementingFunction, args = (),)
-    decrementing = Thread(target = decrementingFunction, args = (),)
+    incrementing = Thread(target = incrementingFunction, args = ());
+    decrementing = Thread(target = decrementingFunction, args = ());
     
     # TODO: Start both threads
+    incrementing.start()
+    decrementing.start()
     
     incrementing.join()
     decrementing.join()
